6ef5480361ebecfcc886c7382b80afd899a9b694
Liu Yize <yize.liu.lyz@gmail.com>
2025-02-16 14:23:54 -0800

    25/2/16 initialization

--- LectureExercise/lec3_lists1/List1Exercises.java
+++ LectureExercise/lec3_lists1/List1Exercises.java
@@ -5,15 +5,49 @@ public class List1Exercises {
       * each element incremented by x. L is not allowed
       * to change. */
     public static IntList incrList(IntList L, int x) {
-        /* Your code here. */
-        return L;        
+        /*
+        // 1 rd
+        IntList returnlist = new IntList(L.first, null);
+        // 2 rd
+        returnlist.rest = new IntList(L.rest.first, null);
+        // 3 rd
+        returnlist.rest.rest = new IntList(L.rest.rest.first, null);
+
+        temp1 = returnlist;
+        temp1 = temp1.rest;
+        temp2 = L;
+        temp2 = temp2.rest;
+
+        temp1 = new intlist(temp2.first, null)
+        */
+        // initialize the first node
+        IntList returnlist = new IntList(L.first + x, null);
+        IntList tempOutput = returnlist;
+        // start with 2nd node
+        IntList tempInput = L.rest;
+        // if next node in example list is not null, copy value
+        while(tempInput != null){
+            tempOutput.rest = new IntList(tempInput.first + x, null);
+            tempOutput = tempOutput.rest;
+            tempInput = tempInput.rest;
+        }
+
+        return returnlist;
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
+        IntList temp = L;
+        temp.first += x;
+        // if list have next node
+        while(temp.rest != null){
+            // move to next node
+            temp = temp.rest;
+            // change value in next node
+            temp.first += x;
+        }
         return L;
     }
 
@@ -24,12 +58,41 @@ public class List1Exercises {
 
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
+        //System.out.println(L.get(1));
 
-        // Test your answers by uncommenting. Or copy and paste the
-        // code for incrList and dincrList into IntList.java and
-        // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        /** Test your answers by uncommenting. Or copy and paste the
+        code for incrList and dincrList into IntList.java and
+        run it in the visualizer. */
+
+        IntList test1 = incrList(L, 3);
+        IntList test2 = dincrList(L, 3);
+        /*
+        for(int i = 0; i < test1.size(); i++){
+            System.out.println(i + "th is " + test1.first);
+            test1 = test1.rest;
+        }
+
+        for(int i = 0; i < test2.size(); i++){
+            System.out.println(i + "th is " + test2.first);
+            test2 = test2.rest;
+        }
+
+         */
+
+        // Print test1 (incrList result)
+        System.out.println("incrList result:");
+        IntList temp1 = test1;
+        for (int i = 0; temp1 != null; i++) {
+            System.out.println(i + "th is " + temp1.first);
+            temp1 = temp1.rest;
+        }
+
+        // Print test2 (dincrList result)
+        System.out.println("dincrList result:");
+        IntList temp2 = test2;
+        for (int i = 0; temp2 != null; i++) {
+            System.out.println(i + "th is " + temp2.first);
+            temp2 = temp2.rest;
+        }
     }
 }
